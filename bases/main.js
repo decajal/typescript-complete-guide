31aab849a068efa06d27c44c59b22dd3539dbeec
Diego Cajal <decajalperez@teco.com.ar>
2024-02-22 10:13:00 -0300

    fix: agrega archivos pendientes de sección 10

--- bases/main.js
+++ bases/main.js
@@ -1,62 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['super velocidad', 'viajar en el tiempo'],
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length > 3 ? true : false;
     };
-    console.log(flash);
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super velocidad', 'Super fuerza'],
-        getName() {
-            return this.name;
-        },
+    Validations.validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutanPower() {
-            return `${this.name} ${this.realName}`;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'diego',
-        age: 47,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa',
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        },
-    };
-    const client2 = {
-        name: 'melisa',
-        age: 30,
-        address: {
-            city: 'Toronto',
-            id: 120,
-            zip: 'K2S U2A',
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        },
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('fernando'));
 //# sourceMappingURL=main.js.map
